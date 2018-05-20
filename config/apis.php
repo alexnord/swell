71247bf4387392561139c0ed2f79f93aca72839f
Alex Nordlinger <alex.nordlinger@gmail.com>
2018-05-20 11:30:19 -0700

    Form and list working

---
+++ config/apis.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'noaa' => 'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=20180519&end_date=20180520&datum=MLLW&station=9410840&time_zone=lst_ldt&units=english&interval=hilo&format=json',
+
+    'stormglass' = [
+    	'url' => 'https://api.stormglass.io/forecast?lat=33.9032750&lng=-118.4235020',
+    ],
+
+];
